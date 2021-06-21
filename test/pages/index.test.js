49e3379f7df4dbd87517e5d8110ea4050c920ea1
Nattan Kifoyi <kifoyinattan@gmail.com>
2021-06-21 19:10:16 +0200

    Adding test environement

---
+++ test/pages/index.test.js
@@ -0,0 +1,8 @@
+// test/pages/index.test.js
+// Using render and screen from test-utils.js instead of
+// @testing-library/react
+
+describe('HomePage', () => {
+  it('should render the heading', () => {
+  })
+})
