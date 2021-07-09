8ff2344114ef778c0ae0bec203d11de4c8e07392
Nattan Kifoyi <kifoyinattan@gmail.com>
2021-07-09 22:02:21 +0200

    delete Home scss

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.scss'
 import TripleColumnItems from '../components/elements/TripleColumnItems'
 import { Fragment } from 'react'
 
