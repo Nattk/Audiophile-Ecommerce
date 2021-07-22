2b4c02979092ee9c46267371456cb42f947a5613
Nattan Kifoyi <kifoyinattan@gmail.com>
2021-07-23 00:29:00 +0200

    footer start and double column component added

--- components/Layouts/Footer/Footer.js
+++ components/Layouts/Footer/Footer.js
@@ -1,9 +1,35 @@
 import layout from '../Layout.module.scss'
+import classes from './Footer.module.scss'
+import Link from 'next/link'
 
 function Footer () {
   return (
-        <footer className={`${layout.footer}`}>
-            <p>Footer</p>
+        <footer className={`${layout.footer} ${classes.footer}`}>
+            <div>
+              <p>Audiophile</p>
+              <p>Audiophile is an all in one stop to fulfill your audio needs.
+                We're a small team of music lovers and sound specialists who are devoted to helping you get the most out of personal audio.
+                Come and visit our demo facility - we’re open 7 days a week.</p>
+            </div>
+            <div>
+            <nav className={`${classes.nav}`}>
+                <p>audiophile</p>
+                <ul>
+                    <li>
+                        <Link href="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link href="/">Headphones</Link>
+                    </li>
+                    <li>
+                        <Link href="/">Speakers</Link>
+                    </li>
+                    <li>
+                        <Link href="/">Earphones</Link>
+                    </li>
+                </ul>
+            </nav>
+            </div>
         </footer>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import TripleColumnItems from '../components/elements/TripleColumnItems'
 import { Fragment } from 'react'
 import TripleRowItems from '../components/elements/TripleRowItems'
+import DoubleColumnItems from '../components/elements/DoubleColumnItems'
 
 export default function Home () {
   return (
@@ -13,6 +14,7 @@ export default function Home () {
       </Head>
         <TripleColumnItems/>
         <TripleRowItems/>
+        <DoubleColumnItems/>
     </Fragment>
 
   )
